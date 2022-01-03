aa00aa662bd781236ddef1dc59a51c7eafc36d65
Wassim Triki <wsmtriki@gmail.com>
2022-01-03 20:14:03 +0100

    add resposivity

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCar } from "react-icons/fa";
 import { BsArrowUpRight, BsArrowBarRight } from "react-icons/bs";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { IoCloseOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 const Navbar = () => {
+  const [isMobileView, setIsMobileView] = useState(true);
+  const [isVisible, setIsVesible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobileView(!isMobileView);
+    setIsVesible(!isVisible);
+  };
   return (
     <nav className="navbar">
+      {isMobileView ? (
+        <AiOutlineMenuUnfold className="menu-icon" onClick={toggleMenu} />
+      ) : (
+        <IoCloseOutline className="menu-icon" onClick={toggleMenu} />
+      )}
       <Link to={"/"}>
         <div className="brand">
           <FaCar className="brand__logo" />
           <span className="brand__name">Garagy</span>
         </div>
       </Link>
-      <ul className="list">
+      <ul className={isVisible ? "list visible" : "list"}>
         <li className="list__item">
           <Link to="/">Home</Link>
         </li>
@@ -25,7 +39,7 @@ const Navbar = () => {
         <li className="list__item">
           <Link to="/contact">Contact</Link>
         </li>
-        <li className="list__item">
+        <li className="list__item login">
           <Link to="/login">Login</Link>
         </li>
       </ul>
