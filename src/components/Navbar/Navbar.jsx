9519c7dd79546dffe3946984e59d472db758dabf
Wassim Triki <wsmtriki@gmail.com>
2022-01-07 18:19:51 +0100

    add breakpoints

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,50 +9,51 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
 const Navbar = () => {
-  const [isVisible, setIsVesible] = useState(false);
+  const [isVisible, setTabletView] = useState(false);
+  const toggleMenu = () => {
+    setTabletView(!isVisible);
+  };
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   const [search, setSearch] = useState("");
   const [dropDownVisible, setDropDownVisible] = useState(false);
+  const toggledropDown = () => {
+    setDropDownVisible(!dropDownVisible);
+  };
   const [expandedSearch, setExpandedSearch] = useState(false);
   const toggleSearch = (e) => {
     e.preventDefault();
     setExpandedSearch(!expandedSearch);
+    searchRef && searchRef.current.focus();
   };
+
   const ref = useRef(null);
+  const searchRef = useRef(null);
   useOnClickOutside(ref, () => {
     setDropDownVisible(false);
     setExpandedSearch(false);
   });
-  // useOnClickOutside(searchRef, () => setDropDownVisible(false));
 
-  const toggledropDown = () => {
-    setDropDownVisible(!dropDownVisible);
-  };
-  let isMobileView = screenWidth <= 500;
-  let isTabletView = screenWidth <= 950;
+  let maxMobile = screenWidth <= 700;
+  let maxTablet = screenWidth <= 900;
+  console.log(maxTablet);
+  let minLaptop = screenWidth <= 1199;
+  let minDesktop = screenWidth > 1200;
   const resizeOnWidth = (e) => {
     setScreenWidth(window.innerWidth);
-    !isTabletView && setIsVesible(false);
   };
-
   useEffect(() => {
     window.addEventListener("resize", resizeOnWidth);
+    minDesktop && setExpandedSearch(false);
     return () => {
       window.removeEventListener("resize", resizeOnWidth);
     };
   });
-  useEffect(() => {
-    console.log(expandedSearch);
-  }, [expandedSearch]);
 
-  const toggleMenu = () => {
-    setIsVesible(!isVisible);
-  };
   return (
     <nav className="navbar">
       <div className="menu-icon-container">
-        {isTabletView &&
+        {maxTablet &&
           (isVisible ? (
             <IoCloseOutline className="menu-icon close" onClick={toggleMenu} />
           ) : (
@@ -69,81 +70,92 @@ const Navbar = () => {
         </div>
       </Link>
 
-      <form ref={ref} className={`form${expandedSearch ? " expanded" : ""}`}>
-        <div className="form__buttons">
-          <button
-            type="submit"
-            className="form-btn form-btn-search"
-            onClick={toggleSearch}
+      <ul className={`list${isVisible ? " visible" : ""}`}>
+        <li className={`list__item${expandedSearch ? " li__expanded" : ""}`}>
+          <form
+            ref={ref}
+            className={`form${expandedSearch ? " expanded" : ""}`}
           >
-            <FiSearch className="search" />
-          </button>
-          <button
-            type="button"
-            onClick={toggledropDown}
-            className="form-btn form-btn-arrow"
-          >
-            <RiArrowDropDownFill className="arrow" />
-          </button>
-        </div>
-        <input
-          className="form__search"
-          value={search}
-          type="text"
-          placeholder="Search"
-          onChange={(e) => setSearch(e.target.value)}
-          onFocus={() => setDropDownVisible(true)}
-        />
-        {dropDownVisible && (
-          <ul className="dropDown">
-            <li onClick={toggledropDown} className="dropDown__item">
-              <FaCarSide />
-              <div className="dropDown__item__text">
-                <p className="dropDown__item__title">Cars</p>
-                <p className="dropDown__item__desc">
-                  Browse available cars for rental.
-                </p>
-              </div>
+            <div className="form__buttons">
+              <button
+                type="submit"
+                className="form-btn form-btn-search"
+                onClick={(e) => {
+                  toggleSearch(e);
+                  toggledropDown();
+                }}
+              >
+                <FiSearch className="search" />
+              </button>
+              <button
+                type="button"
+                onClick={() => !maxTablet && toggledropDown}
+                className="form-btn form-btn-arrow"
+              >
+                <RiArrowDropDownFill className="arrow" />
+              </button>
+            </div>
+            <input
+              className="form__search"
+              ref={searchRef}
+              value={search}
+              type="text"
+              placeholder="Search"
+              onChange={(e) => setSearch(e.target.value)}
+              onFocus={() => setDropDownVisible(true)}
+            />
+            {dropDownVisible && (
+              <ul className="dropDown">
+                <li onClick={toggledropDown} className="dropDown__item">
+                  <FaCarSide />
+                  <div className="dropDown__item__text">
+                    <p className="dropDown__item__title">Cars</p>
+                    <p className="dropDown__item__desc">
+                      Browse available cars for rental.
+                    </p>
+                  </div>
+                </li>
+                <li onClick={toggledropDown} className="dropDown__item">
+                  <FaUser />
+                  <div className="dropDown__item__text">
+                    <p className="dropDown__item__title">Customers</p>
+                    <p className="dropDown__item__desc">
+                      Browse looking for cars.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            )}
+          </form>
+        </li>
+        {!expandedSearch && (
+          <>
+            <li className="list__item">
+              <Link to="/about" onClick={toggleMenu}>
+                About
+              </Link>
             </li>
-            <li onClick={toggledropDown} className="dropDown__item">
-              <FaUser />
-              <div className="dropDown__item__text">
-                <p className="dropDown__item__title">Customers</p>
-                <p className="dropDown__item__desc">Browse looking for cars.</p>
-              </div>
+            <li className="list__item">
+              <Link to="/become-a-seller" onClick={toggleMenu}>
+                Become a Seller
+              </Link>
             </li>
-          </ul>
+
+            <li className="list__item login">
+              <Link to="/login" onClick={toggleMenu}>
+                Login
+              </Link>
+            </li>
+          </>
         )}
-      </form>
-      <div className="list-container"></div>
-      {!expandedSearch && (
-        <ul className={`list${isVisible ? " visible" : ""}`}>
-          <li className="list__item">
-            <Link to="/" onClick={toggleMenu}>
-              Home
-            </Link>
-          </li>
-          <li className="list__item">
-            <Link to="/become-a-seller" onClick={toggleMenu}>
-              Become a Seller
-            </Link>
-          </li>
-          <li className="list__item">
-            <Link to="/contact" onClick={toggleMenu}>
-              Contact
-            </Link>
-          </li>
-          <li className="list__item login">
-            <Link to="/login" onClick={toggleMenu}>
-              Login
-            </Link>
-          </li>
-        </ul>
-      )}
+      </ul>
 
       <div className="join__container">
         <Link to="/join" className="link-join">
-          <button className="btn btn-join" onClick={(e) => setIsVesible(false)}>
+          <button
+            className="btn btn-join"
+            onClick={(e) => setTabletView(false)}
+          >
             Join
             <BsArrowBarRight />
           </button>
