afe18b11401b6afc63ba13eb224d32c8594352e4
Wassim Triki <wsmtriki@gmail.com>
2022-01-07 14:07:06 +0100

    refactor form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { BsArrowBarRight } from "react-icons/bs";
 import { AiOutlineMenuUnfold, AiOutlineSearch } from "react-icons/ai";
 import { IoCloseOutline } from "react-icons/io5";
 import { RiArrowDropDownFill } from "react-icons/ri";
+import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
@@ -13,9 +14,16 @@ const Navbar = () => {
 
   const [search, setSearch] = useState("");
   const [dropDownVisible, setDropDownVisible] = useState(false);
-
+  const [expandedSearch, setExpandedSearch] = useState(false);
+  const toggleSearch = (e) => {
+    e.preventDefault();
+    setExpandedSearch(!expandedSearch);
+  };
   const ref = useRef(null);
-  useOnClickOutside(ref, () => setDropDownVisible(false));
+  useOnClickOutside(ref, () => {
+    setDropDownVisible(false);
+    setExpandedSearch(false);
+  });
   // useOnClickOutside(searchRef, () => setDropDownVisible(false));
 
   const toggledropDown = () => {
@@ -34,6 +42,9 @@ const Navbar = () => {
       window.removeEventListener("resize", resizeOnWidth);
     };
   });
+  useEffect(() => {
+    console.log(expandedSearch);
+  }, [expandedSearch]);
 
   const toggleMenu = () => {
     setIsVesible(!isVisible);
@@ -57,76 +68,79 @@ const Navbar = () => {
           <span className="brand__name">Garagy</span>
         </div>
       </Link>
-      <form className="form">
-        <div ref={ref} className="container">
-          <div className="form__buttons">
-            <button type="submit" className="form-btn form-btn-search">
-              <AiOutlineSearch className="search" />
-            </button>
-            <button type="button" onClick={toggledropDown} className="form-btn">
-              <RiArrowDropDownFill className="arrow" />
-            </button>
-          </div>
-          <input
-            className="form__search"
-            value={search}
-            type="text"
-            placeholder="Search"
-            onChange={(e) => setSearch(e.target.value)}
-            onFocus={() => setDropDownVisible(true)}
-          />
-          {dropDownVisible && (
-            <ul className="dropDown">
-              <li onClick={toggledropDown} className="dropDown__item">
-                <FaCarSide />
-                <div className="dropDown__item__text">
-                  <p className="dropDown__item__title">Cars</p>
-                  <p className="dropDown__item__desc">
-                    Browse available cars for rental.
-                  </p>
-                </div>
-              </li>
-              <li onClick={toggledropDown} className="dropDown__item">
-                <FaUser />
-                <div className="dropDown__item__text">
-                  <p className="dropDown__item__title">Customers</p>
-                  <p className="dropDown__item__desc">
-                    Browse looking for cars.
-                  </p>
-                </div>
-              </li>
-            </ul>
-          )}
-        </div>
-      </form>
 
-      <ul className={isVisible ? "list visible" : "list"}>
-        <li className="list__item">
-          <Link to="/" onClick={toggleMenu}>
-            Home
-          </Link>
-        </li>
-        <li className="list__item">
-          <Link to="/explore" onClick={toggleMenu}>
-            Explore
-          </Link>
-        </li>
-        <li className="list__item">
-          <Link to="/become-a-seller" onClick={toggleMenu}>
-            Become a Seller
-          </Link>
-        </li>
-        <li className="list__item">
-          <Link to="/contact" onClick={toggleMenu}>
-            Contact
-          </Link>
-        </li>
-        <li className="list__item login">
-          <Link to="/login" onClick={toggleMenu}>
-            Login
-          </Link>
-        </li>
-      </ul>
+      <form ref={ref} className={`form${expandedSearch ? " expanded" : ""}`}>
+        <div className="form__buttons">
+          <button
+            type="submit"
+            className="form-btn form-btn-search"
+            onClick={toggleSearch}
+          >
+            <FiSearch className="search" />
+          </button>
+          <button
+            type="button"
+            onClick={toggledropDown}
+            className="form-btn form-btn-arrow"
+          >
+            <RiArrowDropDownFill className="arrow" />
+          </button>
+        </div>
+        <input
+          className="form__search"
+          value={search}
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+          onFocus={() => setDropDownVisible(true)}
+        />
+        {dropDownVisible && (
+          <ul className="dropDown">
+            <li onClick={toggledropDown} className="dropDown__item">
+              <FaCarSide />
+              <div className="dropDown__item__text">
+                <p className="dropDown__item__title">Cars</p>
+                <p className="dropDown__item__desc">
+                  Browse available cars for rental.
+                </p>
+              </div>
+            </li>
+            <li onClick={toggledropDown} className="dropDown__item">
+              <FaUser />
+              <div className="dropDown__item__text">
+                <p className="dropDown__item__title">Customers</p>
+                <p className="dropDown__item__desc">Browse looking for cars.</p>
+              </div>
+            </li>
+          </ul>
+        )}
+      </form>
+      <div className="list-container"></div>
+      {!expandedSearch && (
+        <ul className={`list${isVisible ? " visible" : ""}`}>
+          <li className="list__item">
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="list__item">
+            <Link to="/become-a-seller" onClick={toggleMenu}>
+              Become a Seller
+            </Link>
+          </li>
+          <li className="list__item">
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact
+            </Link>
+          </li>
+          <li className="list__item login">
+            <Link to="/login" onClick={toggleMenu}>
+              Login
+            </Link>
+          </li>
+        </ul>
+      )}
+
       <div className="join__container">
         <Link to="/join" className="link-join">
           <button className="btn btn-join" onClick={(e) => setIsVesible(false)}>
