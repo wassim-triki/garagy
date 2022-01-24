9ac620f60f10ad17a08677aa5b68917d0e375427
Wassim Triki <wsmtriki@gmail.com>
2022-01-24 20:03:51 +0100

    firebase hosting

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("hello world");
+});
+
+exports.api = functions.https.onRequest(app);
